0e7af507ea3b14684fcd73c0112ead64b39c1459
kevinma <2613341+kevinma2010@users.noreply.github.com>
2025-03-02 13:38:22 +0800

    chore: Update webpack and dependencies to latest versions
    - Upgrade webpack from 5.80.0 to 5.94.0
    - Remove deprecated dependencies and update package references
    - Update webpack-related dependencies to match new webpack version
    - Add webpack public path configuration

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, `dist/${browser}`),
       filename: '[name].js',
+      publicPath: '/',
     },
     module: {
       rules: [
